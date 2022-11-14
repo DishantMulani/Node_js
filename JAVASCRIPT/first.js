7ebbd7d776ad6a5be446d9e6b18e8b836865eeae
Dishant Mulani <dishantmulani@gmail.com>
2022-11-14 18:16:00 +0530

    first

---
+++ JAVASCRIPT/first.js
@@ -0,0 +1,9 @@
+var a = 1, b = 2, c = 3, d = 4, e = 5;
+if (a < e) {
+    console.log(e, "is greater than", a);
+}
+console.log("\n");
+for (i = a; i <= e; i++) {
+    
+    console.log(" ",i*i);
+}
---
+++ JAVASCRIPT/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+if (a < e) {
+//     console.log(e, "is greater than", a);
+// }
+// console.log("\n\n");
