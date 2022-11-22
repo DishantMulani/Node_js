332ec689e99a84b67d067c8d24e30e81e06249c3
Dishant Mulani <dishantmulani@gmail.com>
2022-11-22 18:36:06 +0530

    22/11/2022

--- JAVASCRIPT/first.js
+++ JAVASCRIPT/first.js
@@ -1,9 +1,13 @@
-var a = 1, b = 2, c = 3, d = 4, e = 5;
-if (a < e) {
-    console.log(e, "is greater than", a);
-}
-console.log("\n");
-for (i = a; i <= e; i++) {
+// var a = 1, b = 2, c = 3, d = 4, e = 5;
+// if (a < e) {
+//     console.log(e, "is greater than", a);
+// }
+// console.log("\n");
+// for (i = a; i <= e; i++) {
     
-    console.log(" ",i*i*i);
-}
+//     console.log(" ",i*i*i);
+// }
+
+
+let arr=[0,1,2,3,4,5,6,7,8,9,10];
+console.log(arr.slice(-3,-5));
---
+++ JAVASCRIPT/function.js
@@ -0,0 +1,63 @@
+// 1. Name Function
+// ----------------------
+// <-- ex : 1 -->
+// function fun_one(arg1,arg2,arg3){
+//     console.log(arg1,arg2,arg3);
+// }
+//       Input                        Output
+// fun_one("A","B","C");    ------->  A B C
+// fun_one("A","B");        ------->  A B undefined
+// fun_one();               ------->  undefined undefined undefined
+// fun_one(undefined,"A");  ------->  undefined A undefined
+// fun_one(null,null,null); ------->  null null null
+// fun_one(null,"  ",null); ------->  null      null
+// fun_one((),{},[]);       ------->  () {} []
+
+// <-- ex : 2 -->
+//         Input
+// function fun_one(){
+//     console.log("inside fun_one()");
+//     return "Hello";
+// }
+// function fun_two(){
+//     console.log("inside fun_two()");
+//     return fun_one();
+// }
+// console.log(fun_two());
+
+//         Output
+// inside fun_two()
+// inside fun_one()
+// hello
+
+// <-- ex : 3 -->
+//         Input
+// function fun_one(){
+//     return "Hello";
+// }
+// function fun_two(){
+//     return fun_one;
+// }
+// console.log(fun_two()());
+
+//         Output
+// hello
+
+// <-- ex : 4 -->
+//         Input
+// function fun_one(arg1,arg2,arg3){
+//     console.log(arg1(),arg2(),arg3());
+// }
+// function fun_two(){
+//     return "Hello_1";
+// }
+// function fun_three(){
+//     return "Hello_2";
+// }
+// function fun_four(){
+//     return "Hello_3";
+// }
+// fun_one(fun_two,fun_three,fun_four);
+
+//         Output
+// Hello_1 Hello_2 Hello_3
