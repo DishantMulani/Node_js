b72fc6742f2169c4f50140583e575f6d1e32bdcf
Dishant Mulani <dishantmulani@gmail.com>
2022-11-23 22:30:10 +0530

    23-11-2022

---
+++ JAVASCRIPT/rev_number.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")();
+var rem;
+var rev = 0;
+var n;
+
+
+function reverse(n) {
+    console.log(n);
+    while (n > 0) {
+        rem = n % 10;
+        rev = (rev * 10) + rem;
+        n = Math.floor(n /= 10);
+    }
+    return rev;
+}
+n = parseInt(prompt("Enter Number : "));
+console.log(reverse(n));
+
