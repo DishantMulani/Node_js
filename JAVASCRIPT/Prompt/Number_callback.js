00479292db4fa97a92d0199fe091bec6610671f6
Dishant Mulani <dishantmulani@gmail.com>
2022-12-01 00:04:53 +0530

    30-11-2022

---
+++ JAVASCRIPT/Prompt/Number_callback.js
@@ -0,0 +1,78 @@
+
+// let sumof = number => {
+//     let sum = 0;
+//     for (let i = 0; i <= number.length; i++)
+//         sum = sum + number[i];
+//     check(number);
+// }
+
+// let check = number => {
+//     if (number < 9) {
+//         console.log(number);
+//     }
+//     else {
+//         sumof(number);
+//     }
+// }
+// let total = (number, callback) => {
+//     return callback(number);
+// }
+
+// let number = 9898989898;
+// console.log(total(number, sumof));
+
+
+
+let sumof = (n) => {
+
+    let sum = 0;
+    while (n != 0) {
+        sum = sum + n % 10;
+        n = parseInt(n / 10);
+    }
+    return sum;
+}
+
+let check = (value) => {
+    if (value >= 9) {
+        return value;
+    }
+    else {
+        return 0;
+    }
+}
+
+function contSum(number, callback) {
+    let n = callback(number)
+    if (n > 9) {
+        let nu = sumof(n);
+        return sumof(nu);
+    }
+}
+
+let number = 9898989898;
+console.log(contSum(number, sumof));
+
+
+
+// let a = "9898989898";
+// let conArr = number => {
+//     let intarr = Array.from(String(number), Number)
+//     console.log(intarr);
+//     sumOf(intarr);
+// }
+// let sumOf = n => {
+//     let sum = 0;
+//     for (i = 0; i < n.length; i++) {
+//         sum = sum + n[i];
+//         console.log(sum);
+//     }
+//     if (sum > 10) {
+//        return conArr(sum);
+//     }
+//     else {
+//         return sum;
+//     }
+// }
+// let val = conArr(a);
+// console.log(val);
--- JAVASCRIPT/Prompt/tempCodeRunnerFile.js
+++ JAVASCRIPT/Prompt/tempCodeRunnerFile.js
@@ -1,3 +1,19 @@
-1,0));
-// console.log([1, 2, 3, 4, 5,6,7,8,9,10].copyWithin(1,1));
-// console.log([1, 2, 3, 4, 5,6,7,8,9,10].copyWithin(2,2));
+
+let sumof = number => {
+    sum = 0
+    i = 0; i <= number.length; i++
+    sum = sum + number[i];
+    check(number);
+}
+
+let check = number => {
+    if (number < 9) {
+        console.log(number);
+    }
+    else {
+        sumof(number);
+    }
+}
+let total = (number, callback) => {
+    return callback(number);
+}
